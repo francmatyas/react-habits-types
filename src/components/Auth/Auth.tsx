a9d7119e78c9da236a90d3d93f2657e923c36f08
Matyas Franc <franc.matyas@gmail.com>
2023-04-20 23:15:28 +0200

    auth

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -9,18 +9,34 @@ export default function Auth() {
   async function login(data: any) {
     setLoading(true);
     try {
-      const authData = await pb.collection("users").authWithPassword(data.username, data.password);
+      const authData = await pb
+        .collection("users")
+        .authWithPassword(data.username, data.password);
     } catch (error) {
-        alert(error);
+      alert(error);
     }
     setLoading(false);
+  }
+
+  async function signup() {
+    try {
+      const data = {
+        email: "",
+        password: "",
+      };
+
+      const createUser = await pb.collection("users").create(data);
+      await login(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <div className="Auth">
       <h1>Auth</h1>
-        <p>Is valid: {pb.authStore.isValid && pb.authStore.model?.email}</p>
-        {isLoading && <p>Loading ...</p>}
+      <p>Is valid: {pb.authStore.isValid && pb.authStore.model?.email}</p>
+      {isLoading && <p>Loading ...</p>}
       <form onSubmit={handleSubmit(login)}>
         <input type="text" placeholder="username" {...register("username")} />
         <input
@@ -28,7 +44,9 @@ export default function Auth() {
           placeholder="password"
           {...register("password")}
         />
-        <button type="submit" disabled={isLoading}>{isLoading ? "Loading" : "Login"}</button>
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? "Loading" : "Login"}
+        </button>
       </form>
     </div>
   );
